d307eaaea0fa2c488a233560dda4a56e927426d7
Rusty Nicovich <rustynicovich@gmail.com>
2020-06-10 21:36:43 -0700

    changing example_script.m

--- Example_Script.m
+++ Example_Script.m
@@ -7,7 +7,7 @@
 % Copyright (2018) Thanh-An Pham (thanh-an.pham@epfl.ch)
 %                  Emmanuel Soubies (esoubies@gmail.com)
 %-----------------------------------------------------------
-clear, close all;
+% clear, close all;
 
 %% Parameters
 fov=6400;         % Field of view to generate points (nm)     
@@ -17,13 +17,40 @@ Nsamples = 200;   % Number of points in the FRC curve
 fmax = 0.1;       % Maximal frequency. The FRC will be computed on [0,fmax]
 
 %% Load data
-gt=rand(Nmol,2)*fov;
-loc=gt+randn(Nmol,2)*stdMol;
+% gt=rand(Nmol,3)*fov;
+% loc=gt+randn(Nmol,3)*stdMol;
+
+gt=rand(Nmol,3)*fov;
+
+nDetectsPerPoint = 10;
+
+nPtsActual = repmat(nDetectsPerPoint, [Nmol, 1]);
+nPtsActual = poissrnd(nPtsActual);
+pts = zeros(sum(nPtsActual(:)), 4); %[x, y, z, nPhotons, locPrecision]
+ptsNow = 1;
+for k = 1:size(nPtsActual, 1)
+
+    precHere = stdMol.^2;
+    
+    pts(ptsNow:(ptsNow + nPtsActual(k)-1),:) = ...
+        [gt(k,1) + precHere.*randn(nPtsActual(k), 1), ...
+         gt(k,2) + precHere.*randn(nPtsActual(k), 1), ...
+         gt(k,3) + precHere.*randn(nPtsActual(k), 1), ...
+         repmat(precHere, [nPtsActual(k), 1])] ;
+     
+     ptsNow = ptsNow + nPtsActual(k);
+    
+end
+
+%% Split
+pts = pts(randperm(size(pts, 1)), :);
+s1 = pts(1:2:end, :);
+s2 = pts(2:2:end, :);
 
 %% FRC computation
-tic;[FRCcLook,freq] = FRCcont(loc,gt,fov,Nsamples,fmax,1);
+tic;[FRCcLook,freq] = FRCcont(s1,s2,fov,Nsamples,fmax,1);
 disp(['Computation with Lookup table : ',num2str(toc),' s']);
-tic;[FRCcNoLook,freq] = FRCcont(loc,gt,fov,Nsamples,fmax,0);
+tic;[FRCcNoLook,freq] = FRCcont(s1,s2,fov,Nsamples,fmax,0);
 disp(['Computation without Lookup table : ',num2str(toc),' s']);
 
 %% Plots
@@ -31,5 +58,5 @@ figure;hold all;
 plot(freq,FRCcLook,'linewidth',1.5);
 plot(freq,FRCcNoLook,'linewidth',1.5,'linestyle','--');grid
 xlabel('f'); ylabel('FRC'); set(gca,'FontSize',14);
-legend('Lookup Table','No Lookup Table');axis([0 0.1 0 1]);
+legend('Lookup Table','No Lookup Table');axis([0 0.1 -0.05 1]);
 
